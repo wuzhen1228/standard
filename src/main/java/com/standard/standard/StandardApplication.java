7223ccaa625e6931e9f50b4ea7375ad63c9a2381
wuzhen <wuzhen@brtpawn.com>
2020-05-18 12:38:07 +0800

    springboot

---
+++ src/main/java/com/standard/standard/StandardApplication.java
@@ -0,0 +1,13 @@
+package com.standard.standard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandardApplication.class, args);
+	}
+
+}
